e21f0deb4aa4195bbbdcf7dce73b74f6f309bb3f
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2023-10-15 18:00:53 -0500

    Calculo de horas y atrasos por cada usuario y presentado por todos los usuarios, filtrado por lugar de trabajo

--- src/routers/asistencias.py
+++ src/routers/asistencias.py
@@ -113,142 +113,178 @@ async def obtener_asistencias(request: Request, usuario_codigo, fecha_desde, fec
         return {"error": "S", "mensaje": str(error)}
 
 
-@router.get("/calcular_horas_atrasos")
-async def calcular_horas_atrasos(request: Request, usuario_codigo, fecha_desde, fecha_hasta):
-    token = request.headers.get('token')
+async def empleados_asignados(lugar: str):
+    sql = "SELECT templeado.codigo, templeado.nombres || ' ' || templeado.apellidos AS nombre_completo, talmacen.alm_nomcom FROM comun.tlugaresasignados INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN comun.talmacen ON talmacen.alm_codigo = tcoordenadas.alm_codigo INNER JOIN rol.templeado ON tlugaresasignados.usuario_codigo = templeado.codigo"
+    if lugar not in [None, '', 'N']:
+        sql += f" WHERE talmacen.alm_nomcom LIKE '{lugar}'"
+
+    sql += " ORDER BY nombre_completo"
+
+    with Session(engine2) as session:
+        rows = session.execute(text(sql)).fetchall()
+        objetos = [row._asdict() for row in rows]
+        return objetos
+
+
+async def calcular(request: Request, lugar: str, usuario_codigo, fecha_desde, fecha_hasta):
+
     fecha_desde = unquote(fecha_desde)
     fecha_hasta = unquote(fecha_hasta)
-    # query = f"SELECT entrada, salida FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND EXTRACT(MONTH FROM entrada) = {mes}"
-    # Agregar un día a la fecha hasta y restar un segundo
+
     fecha_hasta = datetime.strptime(fecha_hasta, '%Y/%m/%d')
     fecha_hasta = fecha_hasta + timedelta(days=1) - timedelta(seconds=1)
     query = f"SELECT entrada, salida FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND entrada BETWEEN '{fecha_desde}' AND '{fecha_hasta}'"
+
+    sql = "SELECT templeado.codigo, templeado.nombres || ' ' || templeado.apellidos AS nombre_completo, talmacen.alm_nomcom FROM comun.tlugaresasignados INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN comun.talmacen ON talmacen.alm_codigo = tcoordenadas.alm_codigo INNER JOIN rol.templeado ON tlugaresasignados.usuario_codigo = templeado.codigo"
+    if lugar not in [None, '', 'N']:
+        sql += f" WHERE talmacen.alm_nomcom LIKE '{lugar}'"
+
+    sql += " ORDER BY nombre_completo"
+
+    with Session(engine2) as session:
+        rows = session.execute(text(sql)).fetchall()
+
+    with Session(engine2) as session:
+        rows = session.execute(text(query)).fetchall()
+        if len(rows) == 0:
+            return {
+                "error": "S",
+                "mensaje": "",
+                "objetos": rows,
+            }
+
+        asistencias = [row._asdict() for row in rows]
+
+    query = f"SELECT tturnos.dias_trabajados, tturnos.inicio1, tturnos.fin1, tturnos.inicio2, tturnos.fin2 FROM comun.tlugaresasignados INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN rol.templeado ON tlugaresasignados.usuario_codigo = templeado.codigo INNER JOIN comun.tturnosasignados ON tturnosasignados.usuario_codigo = tlugaresasignados.usuario_codigo INNER JOIN comun.tturnos ON tturnosasignados.turno_codigo = tturnos.codigo WHERE templeado.codigo = {usuario_codigo}"
+
+    with Session(engine2) as session:
+        turnos = session.execute(text(query)).fetchall()
+
+        if len(turnos) == 0:
+            return {
+                "error": "S",
+                "mensaje": "",
+                "objetos": turnos,
+            }
+
+    horas_trabajadas = 0
+    atrasos = 0
+
+    for turno in turnos:
+        dias_trabajados = turno["dias_trabajados"]
+
+        if isinstance(dias_trabajados, list):
+            inicio1 = datetime.strptime(turno["inicio1"], '%H:%M').time()
+            fin1 = datetime.strptime(turno["fin1"], '%H:%M').time()
+            if turno["inicio2"] and turno["fin2"]:
+                inicio2 = datetime.strptime(
+                    turno["inicio2"], '%H:%M').time()
+                fin2 = datetime.strptime(turno["fin2"], '%H:%M').time()
+            else:
+                inicio2 = None
+                fin2 = None
+
+            for asistencia in asistencias:
+                entrada = asistencia["entrada"]
+                salida = asistencia["salida"]
+
+                for rango in dias_trabajados:
+
+                    entrada = asistencia["entrada"]
+                    salida = asistencia["salida"]
+
+                    fecha_inicio = datetime.strptime(
+                        rango["from"], "%Y/%m/%d")
+                    fecha_fin = datetime.strptime(
+                        rango["to"], "%Y/%m/%d") + timedelta(days=1) - timedelta(seconds=1)
+
+                    if fecha_inicio <= entrada <= fecha_fin:
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+
+        else:
+            inicio1 = datetime.strptime(turno["inicio1"], '%H:%M').time()
+            fin1 = datetime.strptime(turno["fin1"], '%H:%M').time()
+            if turno["inicio2"] and turno["fin2"]:
+                inicio2 = datetime.strptime(
+                    turno["inicio2"], '%H:%M').time()
+                fin2 = datetime.strptime(turno["fin2"], '%H:%M').time()
+            else:
+                inicio2 = None
+                fin2 = None
+
+            for asistencia in asistencias:
+                entrada = asistencia["entrada"]
+                salida = asistencia["salida"]
+
+                dia_asistencia = entrada.strftime("%A").lower()
+                dia_asistencia = utils.traducir_dia(dia_asistencia)
+
+                # Verifica si el día de la asistencia está programado para trabajar
+                if dias_trabajados.get(dia_asistencia) == "true":
+                    # Ahora, verifica si la asistencia se registra en ese día
+                    if dia_asistencia == "lunes" and turno["dias_trabajados"].get("lunes") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    elif dia_asistencia == "martes" and turno["dias_trabajados"].get("martes") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    if dia_asistencia == "miercoles" and turno["dias_trabajados"].get("miercoles") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    elif dia_asistencia == "jueves" and turno["dias_trabajados"].get("jueves") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    if dia_asistencia == "viernes" and turno["dias_trabajados"].get("viernes") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    elif dia_asistencia == "sabado" and turno["dias_trabajados"].get("sabado") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                    elif dia_asistencia == "domingo" and turno["dias_trabajados"].get("domingo") == "true":
+                        horas_trabajadas += utils.calcular_horas(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+                        atrasos += utils.calcular_atrasos(
+                            inicio1, fin1, inicio2, fin2, entrada, salida)
+
+    return {
+        "horas_trabajadas": round(horas_trabajadas, 2),
+        "atrasos": round(atrasos, 2),
+    }
+
+
+@router.get("/calcular_horas_atrasos")
+async def calcular_horas_atrasos(request: Request, lugar: str, fecha_desde: str, fecha_hasta: str):
+    token = request.headers.get('token')
 
     try:
         if not utils.verify_token(token):
             raise HTTPException(
                 status_code=401, detail="Usuario no autorizado")
         with Session(engine2) as session:
-            rows = session.execute(text(query)).fetchall()
-            if len(rows) == 0:
-                return {
-                    "error": "S",
-                    "mensaje": "",
-                    "objetos": rows,
-                }
+            empleados = await empleados_asignados(lugar)
 
-            asistencias = [row._asdict() for row in rows]
+            for empleado in empleados:
+                calculos = await calcular(request, lugar, empleado['codigo'], fecha_desde, fecha_hasta)
 
-        query = f"SELECT tturnos.dias_trabajados, tturnos.inicio1, tturnos.fin1, tturnos.inicio2, tturnos.fin2 FROM comun.tlugaresasignados INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN rol.templeado ON tlugaresasignados.usuario_codigo = templeado.codigo INNER JOIN comun.tturnosasignados ON tturnosasignados.usuario_codigo = tlugaresasignados.usuario_codigo INNER JOIN comun.tturnos ON tturnosasignados.turno_codigo = tturnos.codigo WHERE templeado.codigo = {usuario_codigo}"
+                empleado['horas_trabajadas'] = calculos['horas_trabajadas']
+                empleado['atrasos'] = calculos['atrasos']
 
-        with Session(engine2) as session:
-            turnos = session.execute(text(query)).fetchall()
-
-            if len(turnos) == 0:
-                return {
-                    "error": "S",
-                    "mensaje": "",
-                    "objetos": turnos,
-                }
-
-        horas_trabajadas = 0
-        atrasos = 0
-
-        for turno in turnos:
-            dias_trabajados = turno["dias_trabajados"]
-
-            if isinstance(dias_trabajados, list):
-                inicio1 = datetime.strptime(turno["inicio1"], '%H:%M').time()
-                fin1 = datetime.strptime(turno["fin1"], '%H:%M').time()
-                if turno["inicio2"] and turno["fin2"]:
-                    inicio2 = datetime.strptime(
-                        turno["inicio2"], '%H:%M').time()
-                    fin2 = datetime.strptime(turno["fin2"], '%H:%M').time()
-                else:
-                    inicio2 = None
-                    fin2 = None
-
-                for asistencia in asistencias:
-                    entrada = asistencia["entrada"]
-                    salida = asistencia["salida"]
-
-                    for rango in dias_trabajados:
-
-                        entrada = asistencia["entrada"]
-                        salida = asistencia["salida"]
-
-                        fecha_inicio = datetime.strptime(
-                            rango["from"], "%Y/%m/%d")
-                        fecha_fin = datetime.strptime(
-                            rango["to"], "%Y/%m/%d") + timedelta(days=1) - timedelta(seconds=1)
-
-                        if fecha_inicio <= entrada <= fecha_fin:
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-
-            else:
-                inicio1 = datetime.strptime(turno["inicio1"], '%H:%M').time()
-                fin1 = datetime.strptime(turno["fin1"], '%H:%M').time()
-                if turno["inicio2"] and turno["fin2"]:
-                    inicio2 = datetime.strptime(
-                        turno["inicio2"], '%H:%M').time()
-                    fin2 = datetime.strptime(turno["fin2"], '%H:%M').time()
-                else:
-                    inicio2 = None
-                    fin2 = None
-
-                for asistencia in asistencias:
-                    entrada = asistencia["entrada"]
-                    salida = asistencia["salida"]
-
-                    dia_asistencia = entrada.strftime("%A").lower()
-                    dia_asistencia = utils.traducir_dia(dia_asistencia)
-
-                    # Verifica si el día de la asistencia está programado para trabajar
-                    if dias_trabajados.get(dia_asistencia) == "true":
-                        # Ahora, verifica si la asistencia se registra en ese día
-                        if dia_asistencia == "lunes" and turno["dias_trabajados"].get("lunes") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        elif dia_asistencia == "martes" and turno["dias_trabajados"].get("martes") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        if dia_asistencia == "miercoles" and turno["dias_trabajados"].get("miercoles") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        elif dia_asistencia == "jueves" and turno["dias_trabajados"].get("jueves") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        if dia_asistencia == "viernes" and turno["dias_trabajados"].get("viernes") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        elif dia_asistencia == "sabado" and turno["dias_trabajados"].get("sabado") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                        elif dia_asistencia == "domingo" and turno["dias_trabajados"].get("domingo") == "true":
-                            horas_trabajadas += utils.calcular_horas(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-                            atrasos += utils.calcular_atrasos(
-                                inicio1, fin1, inicio2, fin2, entrada, salida)
-
-        return {
-            "horas_trabajadas": round(horas_trabajadas, 2),
-            "atrasos": round(atrasos, 2),
-        }
+            return {"error": "N", "mensaje": "", "objetos": empleados}
 
     except Exception as error:
         return {"error": "S", "mensaje": str(error)}
