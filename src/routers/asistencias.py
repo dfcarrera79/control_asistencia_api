26797a4644a0a19b1e2573e1ee2de56e80b4b9c2
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2023-10-14 21:27:57 -0500

    API implementadas para calcular las horas trabajadas y los atrasos

--- src/routers/asistencias.py
+++ src/routers/asistencias.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from datetime import time
+from urllib.parse import unquote
 import json
 import math
 import unicodedata
@@ -28,8 +29,6 @@ async def registrar_entrada(request: Request):
     data = json.loads(request_body)
     employee_id = data['employee_id']
 
-    print('[EMPLOYEE ID]', employee_id)
-
     sql = f"INSERT INTO comun.tasistencias (usuario_codigo, entrada) VALUES ({employee_id}, NOW()) RETURNING codigo"
 
     try:
@@ -49,8 +48,6 @@ async def registrar_salida(request: Request):
     data = json.loads(request_body)
     employee_id = data['employee_id']
 
-    print('[EMPLOYEE ID]', employee_id)
-
     sql = f"UPDATE comun.tasistencias SET salida = NOW() WHERE codigo = (SELECT codigo FROM comun.tasistencias WHERE usuario_codigo = {employee_id} ORDER BY entrada DESC LIMIT 1) AND salida IS NULL RETURNING codigo"
 
     try:
@@ -63,11 +60,69 @@ async def registrar_salida(request: Request):
         return {"error": "S", "mensaje": str(error)}
 
 
-@router.get("/calcular_horas_atrasos")
-async def calcular_horas_atrasos(request: Request, usuario_codigo, mes):
+@router.get("/obtener_numero_paginas")
+async def obtener_numero_paginas(request: Request, usuario_codigo, fecha_desde, fecha_hasta):
     token = request.headers.get('token')
+    sql = f"SELECT COUNT(*) FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND entrada BETWEEN '{fecha_desde}' AND '{fecha_hasta}'"
 
-    query = f"SELECT entrada, salida FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND EXTRACT(MONTH FROM entrada) = {mes}"
+    try:
+        if not utils.verify_token(token):
+            raise HTTPException(
+                status_code=401, detail="Usuario no autorizado")
+        with Session(engine2) as session:
+            rows = session.execute(text(sql)).fetchall()
+            if len(rows) == 0:
+                return {
+                    "error": "S",
+                    "mensaje": "",
+                    "objetos": rows,
+                }
+
+            return {"error": "N", "mensaje": "", "objetos": rows[0][0]}
+
+    except Exception as e:
+        return {"error": "S", "mensaje": str(e)}
+
+
+@router.get("/obtener_asistencias")
+async def obtener_asistencias(request: Request, usuario_codigo, fecha_desde, fecha_hasta, numero_de_pagina, registros_por_pagina):
+    token = request.headers.get('token')
+    offset = (int(numero_de_pagina) - 1) * int(registros_por_pagina)
+
+    fecha_hasta = datetime.strptime(fecha_hasta, '%Y/%m/%d')
+    fecha_hasta = fecha_hasta + timedelta(days=1) - timedelta(seconds=1)
+
+    query = f"SELECT tasistencias.codigo, templeado.nombres || ' ' || templeado.apellidos AS nombre_completo, talmacen.alm_nomcom as lugar_asignado, entrada, salida FROM comun.tasistencias INNER JOIN rol.templeado ON tasistencias.usuario_codigo = templeado.codigo INNER JOIN comun.tlugaresasignados ON tasistencias.usuario_codigo = tlugaresasignados.usuario_codigo INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN comun.talmacen ON talmacen.alm_codigo = tcoordenadas.alm_codigo WHERE tasistencias.usuario_codigo = {usuario_codigo} AND entrada BETWEEN '{fecha_desde}' AND '{fecha_hasta}' ORDER BY entrada OFFSET {offset} ROWS FETCH FIRST {registros_por_pagina} ROWS ONLY"
+
+    try:
+        if not utils.verify_token(token):
+            raise HTTPException(
+                status_code=401, detail="Usuario no autorizado")
+        with Session(engine2) as session:
+            rows = session.execute(text(query)).fetchall()
+            if len(rows) == 0:
+                return {
+                    "error": "S",
+                    "mensaje": "",
+                    "objetos": rows,
+                }
+            asistencias = [row._asdict() for row in rows]
+            return {"error": "N", "mensaje": "", "objetos": asistencias}
+
+    except Exception as error:
+        return {"error": "S", "mensaje": str(error)}
+
+
+@router.get("/calcular_horas_atrasos")
+async def calcular_horas_atrasos(request: Request, usuario_codigo, fecha_desde, fecha_hasta):
+    token = request.headers.get('token')
+    fecha_desde = unquote(fecha_desde)
+    fecha_hasta = unquote(fecha_hasta)
+    # query = f"SELECT entrada, salida FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND EXTRACT(MONTH FROM entrada) = {mes}"
+    # Agregar un día a la fecha hasta y restar un segundo
+    fecha_hasta = datetime.strptime(fecha_hasta, '%Y/%m/%d')
+    fecha_hasta = fecha_hasta + timedelta(days=1) - timedelta(seconds=1)
+    query = f"SELECT entrada, salida FROM comun.tasistencias WHERE usuario_codigo = {usuario_codigo} AND entrada BETWEEN '{fecha_desde}' AND '{fecha_hasta}'"
 
     try:
         if not utils.verify_token(token):
@@ -83,13 +138,11 @@ async def calcular_horas_atrasos(request: Request, usuario_codigo, mes):
                 }
 
             asistencias = [row._asdict() for row in rows]
-            # print('[ASISTENCIAS]: ', asistencias)
 
         query = f"SELECT tturnos.dias_trabajados, tturnos.inicio1, tturnos.fin1, tturnos.inicio2, tturnos.fin2 FROM comun.tlugaresasignados INNER JOIN comun.tcoordenadas ON tcoordenadas.codigo = tlugaresasignados.coordenadas_codigo INNER JOIN rol.templeado ON tlugaresasignados.usuario_codigo = templeado.codigo INNER JOIN comun.tturnosasignados ON tturnosasignados.usuario_codigo = tlugaresasignados.usuario_codigo INNER JOIN comun.tturnos ON tturnosasignados.turno_codigo = tturnos.codigo WHERE templeado.codigo = {usuario_codigo}"
 
         with Session(engine2) as session:
             turnos = session.execute(text(query)).fetchall()
-            print('[TURNOS]: ', turnos)
 
             if len(turnos) == 0:
                 return {
@@ -103,11 +156,8 @@ async def calcular_horas_atrasos(request: Request, usuario_codigo, mes):
 
         for turno in turnos:
             dias_trabajados = turno["dias_trabajados"]
-            print('[DIAS TRABAJADOS]: ', dias_trabajados)
 
             if isinstance(dias_trabajados, list):
-                print('[ES UNA LISTA]')
-
                 inicio1 = datetime.strptime(turno["inicio1"], '%H:%M').time()
                 fin1 = datetime.strptime(turno["fin1"], '%H:%M').time()
                 if turno["inicio2"] and turno["fin2"]:
@@ -133,7 +183,6 @@ async def calcular_horas_atrasos(request: Request, usuario_codigo, mes):
                             rango["to"], "%Y/%m/%d") + timedelta(days=1) - timedelta(seconds=1)
 
                         if fecha_inicio <= entrada <= fecha_fin:
-                            print('[ENTRDA]: ', entrada)
                             horas_trabajadas += utils.calcular_horas(
                                 inicio1, fin1, inicio2, fin2, entrada, salida)
                             atrasos += utils.calcular_atrasos(
@@ -203,31 +252,3 @@ async def calcular_horas_atrasos(request: Request, usuario_codigo, mes):
 
     except Exception as error:
         return {"error": "S", "mensaje": str(error)}
-
-        # Repite esto para todos los días de la semana necesarios
-
-        # # Verifica si la asistencia está dentro de la primera jornada
-        # if inicio1 <= entrada.time() <= fin1:
-        #     # Calcula las horas trabajadas en la primera jornada
-        #     if salida:
-        #         horas_trabajadas += (salida - entrada).seconds / 3600
-
-        #     # Calcula el atraso (si hay)
-        #     atraso = entrada - \
-        #         datetime.combine(entrada.date(), inicio1)
-        #     if atraso.total_seconds() > 300:  # 300 segundos = 5 minutos
-        #         atrasos += atraso.total_seconds() / 60
-
-        # if inicio2 and fin2:
-        #     # Verifica si la asistencia está dentro de la segunda jornada
-        #     if inicio2 <= entrada.time() <= fin2:
-        #         # Calcula las horas trabajadas en la segunda jornada
-        #         if salida:
-        #             horas_trabajadas += (salida -
-        #                                  entrada).seconds / 3600
-
-        #         # Calcula el atraso (si hay)
-        #         atraso = entrada - \
-        #             datetime.combine(entrada.date(), inicio2)
-        #         if atraso.total_seconds() > 300:  # 300 segundos = 5 minutos
-        #             atrasos += atraso.total_seconds() / 60
